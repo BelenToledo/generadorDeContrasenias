5fe2b7d4082d56b8d71b682c1bf4deb34785e578
BJT <toledobelen.j@gmail.com>
2022-09-28 18:07:23 -0300

    modificacion de arrays y fx

--- JS/script.js
+++ JS/script.js
@@ -18,35 +18,35 @@ const checkMinusculas= $("#minuscula")
 const checkNumeros = $("#numeros")
 const checkSimbolos =$("#simbolos")
 
+let password = []
+///******ARRAYS DECLARADOS*******/
+const arrayMayusculas=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+const arrayMinusculas=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
+const arraySimbolos=["@","#","$","%","^","&","*","(",")","-","_","+","=","!",";",".","/","<",">",",","[","]","{","}"]
+const arrayNumeros=["0","1","2","3","4","5","6","7","8","9"]
+const  arrayLecturaSimple = arrayMayusculas.concat(arrayNumeros)
+const arraySoloLetras= arrayMayusculas.concat(arrayMinusculas)
+const arrayTodos= arraySoloLetras.concat(arrayNumeros).concat(arraySimbolos)
 
-let password = " "
-let arrayMayusculas=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-let arrayMinusculas=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
-let arraySimbolos=["@","#","$","%","^","&","*","(",")","-","_","+","=","!",";",".","/","<",">",",","[","]","{","}"]
-let arrayNumeros=["0","1","2","3","4","5","6","7","8","9"]
+//*************FX SELECCION DE CARACTERES y REGLAS*****************/
 
-const okChecked=(I, array1,array2,array3,array4)=>{
-   if(I.addEventListener("click",()=>{
-   console.log(array1,array2,array3,array4)
-        }))
+const okChecked=(I, array1)=>{
+   I.addEventListener("click",()=>{
+    console.log(array1)
+     })
 }
-
-
-
-// const okChecked = (I)=> {
-// if(I.prop("checked")){
-//     console.log("HOLA")
-// }
-// }
 
 okChecked(checkMayusculas, arrayMayusculas)
 okChecked(checkMinusculas,arrayMinusculas)
-okChecked(checkNumeros, arrayNumeros)
+okChecked (checkNumeros, arrayNumeros)
 okChecked(checkSimbolos,arraySimbolos)
-okChecked(lecturaSimple,arrayMayusculas,arrayNumeros)
-okChecked(soloLetras, arrayMayusculas,arrayMinusculas)
-okChecked(todosLosCaracteres,arrayMayusculas,arrayMinusculas,arrayNumeros,arraySimbolos)
+okChecked(lecturaSimple,arrayLecturaSimple)
+okChecked(soloLetras, arraySoloLetras)
+okChecked(todosLosCaracteres,arrayTodos)
 
+
+
+// console.log(selection())
 // okChecked(doceCaracteres)
 // okChecked(nueveCaracteres)
 // okChecked(seisCaracteres)
