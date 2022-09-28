19b58a3bb8f364b1e387b148942dd912bbe6e4ee
BJT <toledobelen.j@gmail.com>
2022-09-28 11:35:10 -0300

    armado de fx

--- JS/script.js
+++ JS/script.js
@@ -24,3 +24,46 @@ let arrayMayusculas=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ
 let arrayMinusculas=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
 let arraySimbolos=["@","#","$","%","^","&","*","(",")","-","_","+","=","!",";",".","/","<",">",",","[","]","{","}"]
 let arrayNumeros=["0","1","2","3","4","5","6","7","8","9"]
+
+const okChecked=(I, array1,array2,array3,array4)=>{
+   if(I.addEventListener("click",()=>{
+   console.log(array1,array2,array3,array4)
+        }))
+}
+
+
+
+// const okChecked = (I)=> {
+// if(I.prop("checked")){
+//     console.log("HOLA")
+// }
+// }
+
+okChecked(checkMayusculas, arrayMayusculas)
+okChecked(checkMinusculas,arrayMinusculas)
+okChecked(checkNumeros, arrayNumeros)
+okChecked(checkSimbolos,arraySimbolos)
+okChecked(lecturaSimple,arrayMayusculas,arrayNumeros)
+okChecked(soloLetras, arrayMayusculas,arrayMinusculas)
+okChecked(todosLosCaracteres,arrayMayusculas,arrayMinusculas,arrayNumeros,arraySimbolos)
+
+// okChecked(doceCaracteres)
+// okChecked(nueveCaracteres)
+// okChecked(seisCaracteres)
+
+
+
+
+// checkMinusculas.addEventListener("click",()=>{
+    
+//         console.log(arrayMinusculas)
+//     })
+// checkNumeros.addEventListener("click",()=>{
+    
+//         console.log(arrayNumeros)
+//     })
+// checkSimbolos.addEventListener("click",()=>{
+    
+//         console.log(arraySimbolos)
+//     })
+// inputPassword.value= "hola"
