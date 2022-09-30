49ae621031e0bafc189727b2583c8794b251327a
BJT <toledobelen.j@gmail.com>
2022-09-30 16:59:45 -0300

    modificacion de fx

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,11 @@
 // alert("estoy funcionando")
-
+//*********OBJETOS**********/
 const $ = (selector) => document.querySelector(selector)
 const $$ = (selector) => document.querySelectorAll(selector)
+const btns = document.getElementsByTagName("input")
 
 const inputPassword= $(".input-generador")
+const btnGenerador=$(".btn-regenerador")
 
 const doceCaracteres = $("#doceCaracteres")
 const nueveCaracteres = $("#nueveCaracteres")
@@ -20,50 +22,46 @@ const checkSimbolos =$("#simbolos")
 
 let password = []
 ///******ARRAYS DECLARADOS*******/
-const arrayMayusculas=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-const arrayMinusculas=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"]
-const arraySimbolos=["@","#","$","%","^","&","*","(",")","-","_","+","=","!",";",".","/","<",">",",","[","]","{","}"]
-const arrayNumeros=["0","1","2","3","4","5","6","7","8","9"]
-const  arrayLecturaSimple = arrayMayusculas.concat(arrayNumeros)
-const arraySoloLetras= arrayMayusculas.concat(arrayMinusculas)
-const arrayTodos= arraySoloLetras.concat(arrayNumeros).concat(arraySimbolos)
+
+// const  arrayLecturaSimple = arrayMayusculas.concat(arrayNumeros)
+// const arraySoloLetras= arrayMayusculas.concat(arrayMinusculas)
+// const arrayTodos= arraySoloLetras.concat(arrayNumeros).concat(arraySimbolos)
+
+let mayusculas = "A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z"
+let minusculas ="a b c d e f g h i j k l m n ñ o p q r s t u v w x y z"
+let numeros= "0 1 2 3 4 5 6 7 8 9"
+let simbolos = " ! @ $ % ^ & * ( ) - _ = + { } [ ] ; , . < > ? / *"
 
 //*************FX SELECCION DE CARACTERES y REGLAS*****************/
 
-const okChecked=(I, array1)=>{
-   I.addEventListener("click",()=>{
-    console.log(array1)
-     })
+for(const btn of btns){
+     if(btn.addEventListener("click", (e)=>{
+        prueba()
+    })
+) prueba()
+}  
+
+
+const prueba =()=>{
+        for(const input of btns){
+            if (input.checked){
+                console.log(true)
+            } else {
+                        false
+
+                     }
+ }
 }
-
-okChecked(checkMayusculas, arrayMayusculas)
-okChecked(checkMinusculas,arrayMinusculas)
-okChecked (checkNumeros, arrayNumeros)
-okChecked(checkSimbolos,arraySimbolos)
-okChecked(lecturaSimple,arrayLecturaSimple)
-okChecked(soloLetras, arraySoloLetras)
-okChecked(todosLosCaracteres,arrayTodos)
-
-
-
-// console.log(selection())
-// okChecked(doceCaracteres)
-// okChecked(nueveCaracteres)
-// okChecked(seisCaracteres)
+prueba()
 
 
 
 
-// checkMinusculas.addEventListener("click",()=>{
+// btnGenerador.addEventListener("click", function(){
     
-//         console.log(arrayMinusculas)
-//     })
-// checkNumeros.addEventListener("click",()=>{
-    
-//         console.log(arrayNumeros)
-//     })
-// checkSimbolos.addEventListener("click",()=>{
-    
-//         console.log(arraySimbolos)
-//     })
-// inputPassword.value= "hola"
+// })
+
+
+
+
+
