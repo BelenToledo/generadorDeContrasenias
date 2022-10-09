88c0bf2ad414861af90267fa11162255764412d4
BJT <toledobelen.j@gmail.com>
2022-10-09 11:50:36 -0300

    fx de botones generar y regenerar

--- JS/script.js
+++ JS/script.js
@@ -1,130 +1,158 @@
 // alert("estoy funcionando")
 //*********OBJETOS**********/
-const $ = (selector) => document.querySelector(selector)
-const $$ = (selector) => document.querySelectorAll(selector)
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
-const inputPassword= $(".input-generador")
-const btnReGenerador=$(".btn-regenerador")
-const btnGenerador=$(".btn-generador")
+const inputPassword = $(".input-generador");
+const btnReGenerador = $(".btn-regenerador");
+const btnGenerador = $(".btn-generador");
 
-const doceCaracteres = $("#doceCaracteres")
-const nueveCaracteres = $("#nueveCaracteres")
-const seisCaracteres = $("#seisCaracteres")
+const doceCaracteres = $("#doceCaracteres");
+const nueveCaracteres = $("#nueveCaracteres");
+const seisCaracteres = $("#seisCaracteres");
 
-const soloLetras=$("#letras")
-const lecturaSimple=$("#simple")
-const todosLosCaracteres=$("#todos")
+const soloLetras = $("#letras");
+const lecturaSimple = $("#simple");
+const todosLosCaracteres = $("#todos");
 
-const checkMayusculas =$("#mayuscula")
-const checkMinusculas= $("#minuscula")
-const checkNumeros = $("#numeros")
-const checkSimbolos =$("#simbolos")
+const checkMayusculas = $("#mayuscula");
+const checkMinusculas = $("#minuscula");
+const checkNumeros = $("#numeros");
+const checkSimbolos = $("#simbolos");
 
-let mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
-let minusculas ="abcdefghijklmnñopqrstuvwxyz"
-let numeros= "0123456789"
-let simbolos = "!@$%^&*()-_=+{}[];,.<>?/*"
-let letras = mayusculas.concat(minusculas)
-let simpleRead = minusculas.concat(numeros)
-let todos = minusculas.concat(mayusculas).concat(numeros).concat(simbolos)
+let mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+let minusculas = "abcdefghijklmnñopqrstuvwxyz";
+let numeros = "0123456789";
+let simbolos = "!@$%^&*()-_=+{}[];,.<>?/*";
+let letras = mayusculas.concat(minusculas);
+let simpleRead = minusculas.concat(numeros);
+let todos = minusculas.concat(mayusculas).concat(numeros).concat(simbolos);
 
-let caracteres=[]
-let password = []
-
+let caracteres = [];
+let password = [];
 
 //*************FX SELECCION DE CARACTERES y REGLAS*****************/
-const okRadioInputs=()=>{
-    if(soloLetras.checked){
-        caracteres=letras
-    } else if(lecturaSimple.checked){
-        caracteres=simpleRead
-    }else if(todosLosCaracteres.checked){
-        caracteres=todos
-    }
-    console.log(caracteres)
-}
+const okRadioInputs = () => {
+  if (soloLetras.checked) {
+    caracteres = letras;
+  } else if (lecturaSimple.checked) {
+    caracteres = simpleRead;
+  } else if (todosLosCaracteres.checked) {
+    caracteres = todos;
+  }
+  console.log(caracteres);
+};
 
-soloLetras.addEventListener("click", okRadioInputs)
-lecturaSimple.addEventListener("click", okRadioInputs)
-todosLosCaracteres.addEventListener("click", okRadioInputs)
+soloLetras.addEventListener("click", okRadioInputs);
+lecturaSimple.addEventListener("click", okRadioInputs);
+todosLosCaracteres.addEventListener("click", okRadioInputs);
 
-const okMayuscula =()=>{
-    if(checkMayusculas.checked){
-        caracteres.push(mayusculas)
-    } else{
-        caracteres = caracteres.filter(element=>{
-            return element !== mayusculas 
-        })
-    }
-    
-    console.log(caracteres)
-    
-}
-checkMayusculas.addEventListener("click", okMayuscula)  
+const okMayuscula = () => {
+  if (checkMayusculas.checked) {
+    caracteres.push(mayusculas);
+  } else {
+    caracteres = caracteres.filter((element) => {
+      return element !== mayusculas;
+    });
+  }
 
-const okMinusculas=()=>{
-    if(checkMinusculas.checked){
-        caracteres.push(minusculas)
-    }else{
-        caracteres = caracteres.filter(element=>{
-            return element !== minusculas
-        })
-    }
-    console.log(caracteres)
-}
-checkMinusculas.addEventListener("click", okMinusculas) 
+  console.log(caracteres);
+};
+checkMayusculas.addEventListener("click", okMayuscula);
 
-const okSimbolos =()=>{
-    if(checkSimbolos.checked){
-        caracteres.push(simbolos)
-    }else{
-        caracteres= caracteres.filter(element=>{
-            return element !== simbolos
-        })
-    }
-    console.log(caracteres)
-}
-checkSimbolos.addEventListener("click",okSimbolos)
+const okMinusculas = () => {
+  if (checkMinusculas.checked) {
+    caracteres.push(minusculas);
+  } else {
+    caracteres = caracteres.filter((element) => {
+      return element !== minusculas;
+    });
+  }
+  console.log(caracteres);
+};
+checkMinusculas.addEventListener("click", okMinusculas);
 
-const okNumeros =()=>{
-    if(checkNumeros.checked){
-        caracteres.push(numeros)
-    }else{
-        caracteres = caracteres.filter(element =>{
-            return element !== numeros
-        })
-    }
-    console.log(caracteres)
-}
-checkNumeros.addEventListener("click",okNumeros)
+const okSimbolos = () => {
+  if (checkSimbolos.checked) {
+    caracteres.push(simbolos);
+  } else {
+    caracteres = caracteres.filter((element) => {
+      return element !== simbolos;
+    });
+  }
+  console.log(caracteres);
+};
+checkSimbolos.addEventListener("click", okSimbolos);
 
-
+const okNumeros = () => {
+  if (checkNumeros.checked) {
+    caracteres.push(numeros);
+  } else {
+    caracteres = caracteres.filter((element) => {
+      return element !== numeros;
+    });
+  }
+  console.log(caracteres);
+};
+checkNumeros.addEventListener("click", okNumeros);
 
 ///*******FX DEL BOTON PARA CORTAR LACONTRASENIA POR CANTIDAD DE CARACTERES Y MOSTRARLA EN EL INPUT */
-const generar=()=>{
-
-    if(doceCaracteres.checked){
-        for(let i= 0; i <parseInt(doceCaracteres.value);i++)
-        password += caracteres[Math.floor(Math.random()*caracteres.length)]
-    }else if( nueveCaracteres.checked){
-        for(let i = 0; i< parseInt(nueveCaracteres.value); i++)
-        password += caracteres[Math.floor(Math.random()*caracteres.length)]
-    }else if(seisCaracteres.checked){
-        for(let i = 0; i<parseInt(seisCaracteres.value); i++)
-        password += carcteres[Math.floor(Math.random()*caracteres.length)]
-
+const generar = () => {
+  let seleccion = caracteres.toString();
+  if (doceCaracteres.checked) {
+    for (let i = 0; i < parseInt(doceCaracteres.value); i++) {
+      password += seleccion[Math.floor(Math.random() * seleccion.length)];
     }
-    console.log(password)
+    return password;
+  } else if (nueveCaracteres.checked) {
+    for (let i = 0; i < parseInt(nueveCaracteres.value); i++) {
+      password += seleccion[Math.floor(Math.random() * seleccion.length)];
+    }
+    return password;
+  } else if (seisCaracteres.checked) {
+    for (let i = 0; i < parseInt(seisCaracteres.value); i++) {
+      password += seleccion[Math.floor(Math.random() * seleccion.length)];
+    }
+  }
+  return password;
+};
 
-}
+doceCaracteres.addEventListener("click", () => {
+  btnGenerador.addEventListener("click", () => {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  });
+});
 
-doceCaracteres.addEventListener("click", generar)
-nueveCaracteres.addEventListener("click",generar)
-seisCaracteres.addEventListener("click",generar)
+seisCaracteres.addEventListener("click", () => {
+  btnGenerador.addEventListener("click", () => {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  });
+});
 
+nueveCaracteres.addEventListener("click", () => {
+  btnGenerador.addEventListener("click", () => {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  });
+});
 
-
-    btnGenerador.addEventListener("click", generar) 
-    
-    document.querySelector(".input-generador").value = "hola"
- 
+btnReGenerador.addEventListener("click", () => {
+  if (doceCaracteres.checked) {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  } else if (nueveCaracteres.checked) {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  } else if (seisCaracteres.checked) {
+    password = " ";
+    generar();
+    document.querySelector(".input-generador").value = password;
+  }
+});
